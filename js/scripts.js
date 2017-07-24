0457494b01e9014916f1551f42d0f98640e75994
David Cuthell and Evan Johnson <evan@>
2017-07-24 10:22:40 -0700

    add backend scripts

---
+++ js/scripts.js
@@ -0,0 +1,72 @@
+//Business Logic
+function Shelter(){
+  this.animal = [];
+  this.person = [];
+}
+
+function Animal(petName, age, species){
+  this.petName = petName;
+  this.age = age;
+  this.species = species;
+  this.adopted = false;
+}
+
+function Person(firstName, lastName, location){
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.address = location;
+}
+
+Shelter.prototype.addPet = function(){
+  for (var i = 0; i <arguments.length; i++){
+    this.animal.push(arguments[i]);
+  }
+}
+
+Shelter.prototype.petByName = function(name){
+  for(var i=0; i < this.animal.length; i++){
+    if(this.animal[i].petName === name){
+      return this.animal[i];
+    }
+  }
+  console.log("Animal not found");
+}
+
+Shelter.prototype.allPets = function(){
+  return this.animal;
+}
+
+Shelter.prototype.petBySpecies = function(species){
+  var animals = [];
+  for(var i=0; i < this.animal.length; i++){
+    if(this.animal[i].species === species){
+      animals.push(this.animal[i]);
+    }
+  }
+  return animals;
+}
+
+Shelter.prototype.petByStatus = function(status){
+  var animals = [];
+  for(var i=0; i < this.animal.length; i++){
+    if(this.animal[i].adopted === status){
+      animals.push(this.animal[i]);
+    }
+  }
+  return animals;
+}
+
+//UI
+$(document).ready(function(){
+  ourShelter = new Shelter();
+  var puppy1 = new Animal("charlie", 2, "dog");
+  var puppy2 = new Animal('chocolate', 3, 'dog');
+  var cat = new Animal ('bowser', 2, 'cat');
+  $("#addPet").click(function(){
+    var name = $("#petName").val();
+    var species = $("#petSpecies").val();
+    var age = $("#petAge").val();
+    var newPet = new Animal(name, age, species);
+    ourShelter.addPet(newPet);
+  })
+});
